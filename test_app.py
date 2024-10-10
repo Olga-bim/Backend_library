9d17eb226ea80e9e19783e2a3ff77ebd81412737
Olga <olga.bimstart@gmail.com>
2024-10-10 16:24:04 +0300

    change test_app

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import unittest
 class TestInitialData(unittest.TestCase):
 
     def setUp(self):
-        from backend.app import app, db, create_initial_data
+        from app import app, db, create_initial_data
         self.app = app
         self.app_context = self.app.app_context()
         self.app_context.push()
@@ -11,21 +11,21 @@ class TestInitialData(unittest.TestCase):
         create_initial_data()
 
     def tearDown(self):
-        from backend.app import db
+        from app import db
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
     def test_customers_exist(self):
-        from backend.app import Customer
+        from app import Customer
         self.assertGreater(Customer.query.count(), 0, "Customers not found!")
 
     def test_books_exist(self):
-        from backend.app import Book
+        from app import Book
         self.assertGreater(Book.query.count(), 0, "Books not found!")
 
     def test_loans_exist(self):
-        from backend.app import Loan
+        from app import Loan
         self.assertGreater(Loan.query.count(), 0, "Loans not found!")
 
 if __name__ == '__main__':
